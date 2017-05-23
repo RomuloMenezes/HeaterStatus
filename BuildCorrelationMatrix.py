350746598cbfe93bed7e936c79ae1a2aff8f5f38
RomuloMenezes <romulo.menezes@gmail.com>
2017-05-23 11:26:49 -0400

    Generetion of correlation matrix complete.

--- BuildCorrelationMatrix.py
+++ BuildCorrelationMatrix.py
@@ -2,12 +2,15 @@ import QueueClass
 import pandas as pd
 import numpy as np
 import sys
+import csv
 
 
 def main(csv_file_name, matrix_size_param):
     matrix_size = int(matrix_size_param)
     df_raw_data = pd.read_csv(csv_file_name, parse_dates=['timestamp'])
     curr_window = QueueClass.Queue(matrix_size)
+    lst_output = list()
+    aux_lst = list()
 
     # Creating auxiliary column with the difference between timestamps in seconds
     aux_col = list()
@@ -17,11 +20,13 @@ def main(csv_file_name, matrix_size_param):
     df_raw_data['deltaInSecs'] = aux_col
 
     for location in df_raw_data.locationId.unique():
+        lst_output = []
         aux_df = df_raw_data.loc[df_raw_data['locationId'] == location]
         aux_df.index = range(aux_df.shape[0])
         curr_mode = aux_df.deltaInSecs.mode()
         outer_indices = iter(range(matrix_size - 1, aux_df.shape[0]))
         for outerIndex in outer_indices:
+            aux_lst = []
             for innerIndex in range(matrix_size - 1):  # The final element will be inserted after the loop
                 nb_of_points_to_insert = int(aux_df.deltaInSecs[outerIndex - innerIndex] / curr_mode) - 1
                 if nb_of_points_to_insert > 0:
@@ -32,9 +37,18 @@ def main(csv_file_name, matrix_size_param):
                     break
                 else:
                     curr_window.enqueue(aux_df.mains[outerIndex-innerIndex])
-            if not curr_window.is_empty:  # Enqueueing the 5th value for mains
-                curr_window.enqueue(aux_df.mains[outerIndex - (matrix_size - 1)])
-
+            if not curr_window.is_empty:  # Enqueueing the 5th value for mains and pushing window into output list
+                # Notice that the dequeueing process inverts the order in the queue, which, in the output list, puts
+                # the measures back to its chronological order
+                aux_lst.append(aux_df.status[outerIndex-innerIndex])
+                for i in range(curr_window.size):
+                    aux_lst.append(curr_window.dequeue())
+                aux_lst.append(aux_df.mains[outerIndex - (matrix_size - 1)])
+                lst_output.append(aux_lst)
+        if len(lst_output) > 0:
+            with open("C:\Simptek\Assignment01_20170519\CorrelationMatrix_" + location + ".csv", 'wb') as my_file:
+                wr = csv.writer(my_file)
+                wr.writerows(lst_output)
 
 if __name__ == '__main__':
     main(sys.argv[1], sys.argv[2])
